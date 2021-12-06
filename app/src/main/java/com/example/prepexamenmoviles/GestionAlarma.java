880da7b946d299db63459a9490c682aa024931e1
pablo <pablo.hchez@gmail.com>
2021-12-06 17:37:34 +0100

    Opcion de usar mi gestor de alarmas

---
+++ app/src/main/java/com/example/prepexamenmoviles/GestionAlarma.java
@@ -0,0 +1,14 @@
+package com.example.prepexamenmoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionAlarma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_alarma);
+    }
+}
